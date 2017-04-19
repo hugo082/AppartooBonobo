c0cb7a0dfb47a9f50025f7ed79e03f32da960703
hugo_082 <hugo.fouquet@epita.fr>
2017-04-18 23:00:53 -0400

    Auth - Profile - Firendships

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,64 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/profile/friends/add", name="friends_add")
+     */
+    public function addFriendsAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('username', TextType::class)
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $data = $form->getData();
+            $friend = $em->getRepository('AppBundle:User')
+                ->findOneBy(array('username' => $data['username']));
+            if ($friend) {
+                $user = $this->getUser();
+                if ($friend == $user)
+                    $this->addFlash('warning','Tu ne peux pas t\'ajouter en amis');
+                else {
+                    $user->addMyFriend($friend);
+                    $em->persist($user);
+                    $em->flush();
+                    $this->addFlash(
+                        'notice',
+                        $friend->getUsername() . ' est maintenant ton amis !'
+                    );
+                }
+                return $this->redirectToRoute('fos_user_profile_show');
+            } else
+                $this->addFlash('warning', $data['username'] . ' est introuvable');
+        }
+
+        // replace this example code with whatever you need
+        return $this->render('default/addFriends.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/profile/friends/remove/{id}", name="friends_rm")
+     */
+    public function removeFriendsAction(Request $request, int $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $friend = $em->getRepository('AppBundle:User')->find($id);
+        if ($friend) {
+            $user = $this->getUser();
+            $user->removeMyFriend($friend);
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('notice',$friend->getUsername() . ' n\'est plus ton amis !');
+        } else
+            $this->addFlash('warning','Ton amis est introuvable');
+
+        return $this->redirectToRoute('fos_user_profile_show');
     }
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,236 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer", nullable=true)
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="family", type="string", length=255, nullable=true)
+     */
+    private $family;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="race", type="string", length=255, nullable=true)
+     */
+    private $race;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="food", type="string", length=255, nullable=true)
+     */
+    private $food;
+
+    /**
+     * Many Users have Many Users.
+     * @ORM\ManyToMany(targetEntity="User", mappedBy="myFriends")
+     */
+    private $friendsWithMe;
+
+    /**
+     * Many Users have many Users.
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="friendsWithMe")
+     * @ORM\JoinTable(name="friendships",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="friend_user_id", referencedColumnName="id")}
+     *      )
+     */
+    private $myFriends;
+
+    public function __construct() {
+        parent::__construct();
+        $this->friendsWithMe = new ArrayCollection();
+        $this->myFriends = new ArrayCollection();
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return User
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return integer
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set family
+     *
+     * @param string $family
+     *
+     * @return User
+     */
+    public function setFamily($family)
+    {
+        $this->family = $family;
+
+        return $this;
+    }
+
+    /**
+     * Get family
+     *
+     * @return string
+     */
+    public function getFamily()
+    {
+        return $this->family;
+    }
+
+    /**
+     * Set race
+     *
+     * @param string $race
+     *
+     * @return User
+     */
+    public function setRace($race)
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * Get race
+     *
+     * @return string
+     */
+    public function getRace()
+    {
+        return $this->race;
+    }
+
+    /**
+     * Set food
+     *
+     * @param string $food
+     *
+     * @return User
+     */
+    public function setFood($food)
+    {
+        $this->food = $food;
+
+        return $this;
+    }
+
+    /**
+     * Get food
+     *
+     * @return string
+     */
+    public function getFood()
+    {
+        return $this->food;
+    }
+
+    /**
+     * Add friendsWithMe
+     *
+     * @param \AppBundle\Entity\User $friendsWithMe
+     *
+     * @return User
+     */
+    public function addFriendsWithMe(\AppBundle\Entity\User $friendsWithMe)
+    {
+        $this->friendsWithMe[] = $friendsWithMe;
+
+        return $this;
+    }
+
+    /**
+     * Remove friendsWithMe
+     *
+     * @param \AppBundle\Entity\User $friendsWithMe
+     */
+    public function removeFriendsWithMe(\AppBundle\Entity\User $friendsWithMe)
+    {
+        $this->friendsWithMe->removeElement($friendsWithMe);
+    }
+
+    /**
+     * Get friendsWithMe
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getFriendsWithMe()
+    {
+        return $this->friendsWithMe;
+    }
+
+    /**
+     * Add myFriend
+     *
+     * @param \AppBundle\Entity\User $myFriend
+     *
+     * @return User
+     */
+    public function addMyFriend(\AppBundle\Entity\User $myFriend)
+    {
+        $this->myFriends[] = $myFriend;
+
+        return $this;
+    }
+
+    /**
+     * Remove myFriend
+     *
+     * @param \AppBundle\Entity\User $myFriend
+     */
+    public function removeMyFriend(\AppBundle\Entity\User $myFriend)
+    {
+        $this->myFriends->removeElement($myFriend);
+    }
+
+    /**
+     * Get myFriends
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getMyFriends()
+    {
+        return $this->myFriends;
+    }
+}
